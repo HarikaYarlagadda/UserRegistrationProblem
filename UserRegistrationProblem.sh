078463611d947e88af844dadace04e26a7b72aac
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-11-09 11:34:40 +0530

    entered code for valid lastname

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 read -p "enter first name " FirstName
+read -p "enter last name " LastName
 pat="^[A-Z]{1}[a-z]*$"
-if [[ $FirstName =~ $pat ]]
+if [[ $FirstName =~ $pat && $LastName =~ $pat ]]
 then
 	echo true
 else
