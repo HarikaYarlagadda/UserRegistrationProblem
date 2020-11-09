0cfc2d1c20b6b5a3ec2591045fb1d2edf2612d50
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-11-09 11:38:16 +0530

    entered code for valid Email

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 read -p "enter first name " FirstName
 read -p "enter last name " LastName
+read -p "enter email " Email
 pat="^[A-Z]{1}[a-z]*$"
-if [[ $FirstName =~ $pat && $LastName =~ $pat ]]
+patEmail="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.]+$"
+if [[ $FirstName =~ $pat && $LastName =~ $pat && $Email =~ $patEmail ]]
 then
 	echo true
 else
