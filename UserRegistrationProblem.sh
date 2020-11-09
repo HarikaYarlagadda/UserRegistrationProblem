c4d19af4948a6f61f31818582e7b2380283c8dda
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-11-09 11:40:54 +0530

    entered code for valid Mobile Number

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -2,9 +2,11 @@
 read -p "enter first name " FirstName
 read -p "enter last name " LastName
 read -p "enter email " Email
+read -p "enter mobilenumber with country code " MobileNumber
 pat="^[A-Z]{1}[a-z]*$"
 patEmail="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.]+$"
-if [[ $FirstName =~ $pat && $LastName =~ $pat && $Email =~ $patEmail ]]
+patMobNumber="^[0-9]{2}[ ]?[0-9]{10}$"
+if [[ $FirstName =~ $pat && $LastName =~ $pat && $Email =~ $patEmail && $MobileNumber =~ $patMobNumber ]]
 then
 	echo true
 else
